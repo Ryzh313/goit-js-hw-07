e03269632cffee4228e6507258b6839380817a89
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-16 01:33:18 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,82 +2,68 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-// const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
-// const makeGalleryItem = ({ preview, original, description }) => {
-//     const galleryItem = document.createElement('div');
-//     galleryItem.classList.add('gallery__item');
+// create item
+const makeGalleryItem = ({ preview, original, description }) => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
     
-//     const galleryLink = document.createElement('a');
-//     galleryLink.classList.add('gallery__link');
-//     galleryLink.href.add(`${original}`);
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = `${original}`;
 
-//     const galleryImage = document.createElement('img');
-//     galleryImage.classList.add('gallery__image');
-//     galleryImage.src.add(`${preview}`);
-//     galleryImage.dataset.
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = `${preview}`;
+    galleryImage.dataset.source = `${original}`;
+    galleryImage.alt = `${description}`;
 
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
 
-//     console.log(makeGalleryItem);
-// }
+    return galleryItem;
+    
+};
 
+// create markup
+const elements = galleryItems.map(makeGalleryItem)
+console.log(elements);
 
-const divRef = document.querySelector('.gallery');
-
-//  create gallery markup
-
-function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image"
-                    src="${preview}" 
-                    data-source="${original}" 
-                    alt="${description}"></img>
-    </a>
-</div>`
-    }).join('');
-}
-
-const addGallaryMarkup = createGalleryMarkup(galleryItems);
-
-divRef.innerHTML = addGallaryMarkup;
+galleryContainer.append(...elements);
 
 // add onClick function
 
-divRef.addEventListener('click', onClickImg);
+galleryContainer.addEventListener('click', onClickImg);
 
-function onClickImg(evt) {
-    evt.preventDefault();
-    if (evt.target.nodeName !== 'IMG') {
+function onClickImg(evant) {
+    evant.preventDefault();
+    if (evant.target.nodeName !== 'IMG') {
         return;
-    } else {
-        instance.show();
     }
-    console.log(evt.target.nodeName);    
-}
+    console.log(evant.target.nodeName);    
+};
 
 const instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-`)
+    <img src="${evant.target.dataset.source}">
+`);
 
-divRef.addEventListener("keydown", (event) => {
-    if (event.code === 'Escape') {
-        instance.close()
+// ${evant.target.dataset.source.value} problem
+
+galleryContainer.addEventListener("click", (evant) => {
+    if (evant.target.nodeName === 'IMG') {
+       instance.show();
+    }    
+});
+
+galleryContainer.addEventListener("keydown", (evant) => {
+    if (evant.code === 'Escape') {
+        instance.close();
     }    
 });
 
 
 
-// item.addEventListener('click', (event) => {
-//     instance.show(); // this shows the lightbox
-//   });
 
-// instance.show()
-
-// https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg
-
-// ${evt.target.dataset.source}
 
 
