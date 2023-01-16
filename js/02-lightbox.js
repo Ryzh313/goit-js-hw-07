bd566b4167ea0af62b4690b44392ebe2aa488e2f
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-16 03:42:34 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,20 +9,29 @@ const addGallaryMarkup = createGalleryMarkup(galleryItems);
 
 divRef.insertAdjacentHTML('beforeend', addGallaryMarkup);
 
-
-// markup function
 function createGalleryMarkup(items) {
     return items.map(
         (item) => {
         return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
-                <img class="gallery__image"
-                    src="${item.preview}" 
-                    data-source="${item.original}" 
-                    alt="${item.description}">
-                </img>
-            </a>
-        </div>`
+        <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
     }).join('');
-}
+};
+
+function onClickImg(evant) {
+    evant.preventDefault();
+    if (evant.target.nodeName !== 'IMG') {
+        return;
+    } 
+    console.log(evant.target.nodeName);    
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionType: 'text',
+});
+
